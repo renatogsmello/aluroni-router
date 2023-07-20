fe6947afb1ee40c110cd5e074babad3d68bbec8f
Renato Mello <renatogsmello@gmail.com>
2023-07-20 20:22:03 -0300

    estrutura básica do router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import Cardapio from './pages/Cardapio';
+import React from "react"
+import ReactDOM from "react-dom"
+import "normalize.css"
+import "./index.css"
+import Router from "./routes"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Cardapio />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,9 +1,21 @@
-import cardapio from "../Cardapio/Itens/itens.json"
+import cardapio from "data/cardapio.json"
+import styles from "./Inicio.module.scss"
 export default function Inicio() {
+	let pratosRecomendados = [...cardapio]
+	pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3)
 	return (
 		<section>
-			<h3>Recomendações da cozinha</h3>
-			<div></div>
+			<h3 className={styles.titulo}>Recomendações da cozinha</h3>
+			<div className={styles.recomendados}>
+				{pratosRecomendados.map((item) => (
+					<div key={item.title} className={styles.recomendado}>
+						<div className={styles.recomendado__imagem}>
+							<img src={item.photo} alt={item.title} />
+						</div>
+						<button className={styles.recomendado__botao}>Ver mais</button>
+					</div>
+				))}
+			</div>
 		</section>
 	)
 }
