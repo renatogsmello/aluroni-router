90669ad2df8aeef097105dc4bca712615b90b0e6
Renato Mello <renatogsmello@gmail.com>
2023-07-20 19:16:05 -0300

    first commit

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,9 @@
+import cardapio from "../Cardapio/Itens/itens.json"
+export default function Inicio() {
+	return (
+		<section>
+			<h3>Recomendações da cozinha</h3>
+			<div></div>
+		</section>
+	)
+}
